cf230673eb29e48b439d35e18ea5fb278341d35b
web-dave <dave@webdave.de>
2022-08-29 12:13:31 +0200

    attribute/input binding

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.scss']
+  styleUrls: ['./book-card.component.scss'],
 })
 export class BookCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() content: any;
+  customStyles = {
+    backgroundColor: 'blue',
+  };
+  constructor() {
+    // setTimeout(() => {
+    //   this.customStyles = {
+    //     backgroundColor: 'red',
+    //   };
+    // }, 2000);
   }
 
+  ngOnInit(): void {}
 }
