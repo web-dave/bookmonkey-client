9df1841ee27b095c2d75ef5f812c67060e62c4b0
web-dave <dave@webdave.de>
2022-09-07 17:26:57 +0200

    final

--- src/app/preload-delayed.ts
+++ src/app/preload-delayed.ts
@@ -8,6 +8,7 @@ import { Observable, switchMap, timer } from 'rxjs';
 export class PreloadDelayed implements PreloadingStrategy {
   constructor() {}
   preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    // console.log((window.navigator.connection as any).saveData);
     if (route?.data?.['preload']) {
       return timer(3000).pipe(switchMap(() => fn()));
     } else {
