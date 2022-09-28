da33081d8a6873772208795aa9dcdff032b57604
web-dave <dave@webdave.de>
2022-09-28 11:05:46 +0200

    test Book Preview

--- src/app/books/book-preview/book-preview.component.spec.ts
+++ src/app/books/book-preview/book-preview.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { mockBooks } from 'mocks/mock.service';
+import { IBook } from '../book.interface';
 
 import { BookPreviewComponent } from './book-preview.component';
 
@@ -18,5 +20,16 @@ fdescribe('BookPreviewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should raise bookSeleted event on click', () => {
+    let expectedBook: IBook | undefined;
+    component.book = mockBooks[0];
+    fixture.detectChanges();
+    component.bookSelected.subscribe((data) => (expectedBook = data));
+    component.selectThisBook();
+    console.log(expectedBook);
+
+    expect(expectedBook).toBe(mockBooks[0]);
   });
 });
