032881f02e8e6223f959baf5a4aa75933330e7a5
web-dave <dave@webdave.de>
2022-11-08 12:19:50 +0100

    poperty binding

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.scss']
+  styleUrls: ['./book-card.component.scss'],
 })
 export class BookCardComponent implements OnInit {
+  foo = ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat consequatur
+accusantium, officia quasi repellendus ab dolorum iure accusamus corporis ad
+nisi quod, at, aut doloribus eaque sequi reiciendis laborum distinctio?`;
 
-  constructor() { }
+  myStyle = {
+    color: 'hotpink',
+  };
+  constructor() {}
 
   ngOnInit(): void {
+    setTimeout(() => (this.myStyle.color = '#efefef'), 1500);
   }
-
 }
