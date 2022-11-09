f037757e544e43106fb18b61344f36050aec3d38
web-dave <dave@webdave.de>
2022-11-09 11:21:15 +0100

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,40 @@ import { IBook } from './models/book';
 })
 export class AppComponent {
   title = 'bookmonkey-client-14 🦄';
+  books: IBook[] = [
+    {
+      title: 'How I met your Father',
+      author: 'Max und Maja',
+      abstract: 'äldf kasdöjc nkabö rfö dkv jn fad ögrfu',
+    },
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
 
-  book: IBook = {
-    title: 'How I met your Father',
-    author: 'Max und Maja',
-    abstract: 'äldf kasdöjc nkabö rfö dkv jn fad ögrfu',
-  };
+  status = 'done';
+
+  constructor() {
+    // setTimeout(() => {
+    //   this.book.foo = { bar: 'BAZ' };
+    // }, 2000);
+  }
+  getStatus() {
+    // console.info('Get Status');
+    return this.status;
+  }
 
   goToBookDetails(data: IBook) {
     console.log(data);
--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -2,4 +2,7 @@ export interface IBook {
   title: string;
   author: string;
   abstract: string;
+  foo?: {
+    bar: string;
+  };
 }
