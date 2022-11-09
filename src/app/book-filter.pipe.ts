7b881536d025afaffb7f12ca22d3e803fdc03636
web-dave <dave@webdave.de>
2022-11-09 12:40:44 +0100

    pipe extra

--- src/app/book-filter.pipe.ts
+++ src/app/book-filter.pipe.ts
@@ -6,6 +6,15 @@ import { IBook } from './models/book';
 })
 export class BookFilterPipe implements PipeTransform {
   transform(books: IBook[], searchTerm: string): IBook[] {
+    // const res: IBook[] = [];
+
+    // for (let book of books) {
+    //   if (book.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+    //     res.push(book);
+    //   }
+    // }
+
+    // return res;
     return books.filter((book) =>
       book.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
