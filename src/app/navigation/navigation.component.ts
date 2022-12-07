31851b47a7ee54626a58f2608ef23b78c22b8a9f
web-dave <dave@webdave.de>
2022-12-07 22:10:31 +0100

    login nav

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent {
+  auth$ = this.auth.loggedIn$;
+  constructor(private auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  logOut() {
+    this.auth.logOut();
   }
-
 }
