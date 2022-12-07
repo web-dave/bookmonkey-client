370334e64ed84bb606a40081cdda4a4e00b3547a
web-dave <dave@webdave.de>
2022-12-07 21:59:14 +0100

    provide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -8,10 +8,16 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 
+const providers: Provider[] = [];
 @NgModule({
-  declarations: [AppComponent, AboutComponent, NavigationComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    NavigationComponent,
+    LoginComponent,
+  ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
-  providers: [],
+  providers: [...providers],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
