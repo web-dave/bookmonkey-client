8f4a536b7964e7f46026512e9c472beffeeb7163
web-dave <dave@webdave.de>
2022-12-08 12:57:29 +0100

    reset tests

--- cypress/e2e/books.cy.ts
+++ cypress/e2e/books.cy.ts
@@ -1,45 +1,8 @@
-import { getFormControl } from './util';
-
 describe('Books', () => {
   before(() => {
     cy.visit('http://localhost:4200/books');
   });
 
-  afterEach(() => {
-    cy.request('DELETE', 'http://localhost:4730/books/978-3-453-40780-0');
-  });
-  it('create a book', () => {
-    let bookCount = 0;
-    cy.get('app-book-card').then((books) => {
-      bookCount = books.length;
-      console.log(bookCount);
-      cy.get('[routerLink="new"]').click().contains('➕');
-      cy.contains('Create a Book');
-
-      cy.get('[type="submit"]').should('be.disabled');
-      getFormControl('title').should('have.class', 'ng-invalid');
-      [
-        'title',
-        'abstract',
-        'subtitle',
-        'isbn',
-        'numPages',
-        'publisher',
-        'price',
-        'cover',
-      ].forEach((name) => {
-        getFormControl(name).type(name.toUpperCase());
-      });
-      getFormControl('title').should('have.class', 'ng-valid');
-      cy.get('[type="submit"]').should('be.disabled');
-      getFormControl('isbn')
-        .clear()
-        .type('978-3-453-40780-0')
-        .should('have.class', 'ng-valid');
-
-      cy.get('[type="submit"]').should('be.enabled').click();
-      cy.get('a').contains('Books').click();
-      cy.get('app-book-card').should('have.length', bookCount + 1);
-    });
-  });
+  afterEach(() => {});
+  it('create a book', () => {});
 });
--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -2,7 +2,5 @@ describe('Navigation', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4200/');
   });
-  it('should be visible', () => {
-    cy.get('app-navigation a').should('have.length', 2);
-  });
+  it('should have 3 links', () => {});
 });
