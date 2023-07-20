f1dca3e9a1cbc66b4d698f80a1017bb76be3c9fc
Dave <dave@webdave.de>
2023-07-20 11:13:03 +0200

    property-bindings

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lr-book-card',
   templateUrl: './book-card.component.html',
-  styleUrls: ['./book-card.component.scss']
+  styleUrls: ['./book-card.component.scss'],
 })
 export class BookCardComponent {
+  foo = '<span>Bar</span>';
+  myClass = 'pp';
 
+  myStyle = {
+    color: 'lime',
+    fontSize: '44px',
+  };
+  constructor() {
+    this.say(this.foo);
+  }
+
+  say(word: string) {
+    console.log(word);
+  }
 }
