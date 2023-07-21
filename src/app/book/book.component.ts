6241f466c468358ee6ccce75c32563cbafd3e1fe
Dave <dave@webdave.de>
2023-07-21 11:50:44 +0200

    Observables

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IBook } from './IBook';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'lr-book',
@@ -17,23 +18,14 @@ import { IBook } from './IBook';
 })
 export class BookComponent {
   searchTerm = '';
-  books: IBook[] = [
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...',
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...',
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ...",
-    },
-  ];
+  books: IBook[] = [];
+  constructor(private service: BookService) {
+    this.service.getAll().subscribe({
+      next: (data) => (this.books = data),
+      error: (err) => console.error(err),
+      complete: () => console.info('DONE'),
+    });
+  }
   navigate(e: IBook) {
     console.table(e);
   }
