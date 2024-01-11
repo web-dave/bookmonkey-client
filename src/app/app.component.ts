4c9814cb235bddb19c9877624de11f352ba0f22f
Dave <dave@webdave.de>
2024-01-11 10:14:22 +0100

    1st component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BookCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
