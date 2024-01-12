29e74fffe27ee809fa33dd6cd151595fc625058c
Dave <dave@webdave.de>
2024-01-12 09:30:44 +0100

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,23 @@ import { IBook } from './book';
 })
 export class AppComponent {
   title = 'bookmonkey-client 🤩';
-  book: IBook = {
-    title: 'How to win friends',
-    author: 'Dale Carnegie',
-    abstract: 'In this book ...',
-  };
+  books: IBook[] = [
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
 
   pong(data: IBook) {
     console.table(data);
