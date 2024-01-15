2a0b463a19bf16e899c3a19f8d18fc3ee1077696
Dave <dave@webdave.de>
2024-01-15 12:28:03 +0100

    HttpClient

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,33 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { IBook } from './book';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private books: IBook[] = [
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...',
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...',
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ...",
-    },
-  ];
-  constructor() {
-    console.log('Ich bin Hier');
-  }
+  // private books: IBook[] = [
+  //   {
+  //     title: 'How to win friends',
+  //     author: 'Dale Carnegie',
+  //     abstract: 'How to Win Friends and Influence ...',
+  //   },
+  //   {
+  //     title: 'The Willpower Instinct: How Self-Control Works ...',
+  //     author: 'Kelly McGonigal',
+  //     abstract: 'Based on Stanford University ...',
+  //   },
+  //   {
+  //     author: 'Simon Sinek',
+  //     title: 'Start with WHY',
+  //     abstract: "START WITH WHY shows that the leaders who've ...",
+  //   },
+  // ];
+
+  private http = inject(HttpClient);
+
+  // private baseUrl = 'https://api5.angular-buch.com/books';
+  private baseUrl = 'http://localhost:4730/books';
+  // constructor(private http: HttpClient) {
+  //   console.log('Ich bin Hier');
+  // }
 
   getAll(): Observable<IBook[]> {
-    return of(this.books);
+    // return of(this.books);
+    return this.http.get<IBook[]>(this.baseUrl);
   }
 }
