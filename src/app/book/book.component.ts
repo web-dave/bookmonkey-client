4ca7b21890ef982126ce37a64ff6342d4670e8b7
Dave <dave@webdave.de>
2024-01-16 12:17:20 +0100

    Signal

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,24 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { IBook } from './book';
-import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 import { BookCardComponent } from './book-card/book-card.component';
 import { FilterBooksPipe } from './filter-books.pipe';
 import { BookService } from './book.service';
-import { Observable, catchError, of, tap } from 'rxjs';
+import { catchError, of, tap } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [
-    BookCardComponent,
-    FilterBooksPipe,
-    JsonPipe,
-    NgFor,
-    NgIf,
-    AsyncPipe,
-  ],
+  imports: [BookCardComponent, FilterBooksPipe, JsonPipe, NgFor, NgIf],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss',
 })
@@ -27,19 +21,21 @@ export class BookComponent {
   loading = true;
   error = false;
 
-  books$: Observable<IBook[]> = inject(BookService)
-    .getAll()
-    .pipe(
-      tap({
-        next: (data) => console.log(data),
-        complete: () => console.info('Done'),
-        error: (err) => console.error(err),
-      }),
-      catchError((err: HttpErrorResponse) => {
-        this.error = true;
-        return of([]);
-      })
-    );
+  books = toSignal(
+    inject(BookService)
+      .getAll()
+      .pipe(
+        tap({
+          next: (data) => console.log(data),
+          complete: () => console.info('Done'),
+          error: (err) => console.error(err),
+        }),
+        catchError((err: HttpErrorResponse) => {
+          this.error = true;
+          return of([]);
+        })
+      )
+  );
 
   setSearchTerm(s: string) {
     this.searchTerm = s;
