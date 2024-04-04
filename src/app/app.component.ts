d28dbab2e8520119c7befa27f0e6762d260b2cd0
Dave <dave@webdave.de>
2024-04-04 12:27:22 +0200

    ngFor @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { BookCardComponent } from './book-card/book-card.component';
 import { IBook } from './book.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, BookCardComponent],
+  imports: [CommonModule, BookCardComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'bookmonkey-client ;)';
-  book: IBook = {
-    title: 'How to win friends',
-    author: 'Dale Carnegie',
-    abstract: 'In this book ...',
-  };
+  books: IBook[] = [
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
   pong(event: IBook) {
     console.log(event);
   }
