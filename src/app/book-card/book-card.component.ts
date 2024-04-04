66320864bb5ce81bc6090f9a3b3fba8a07bdaa05
Dave <dave@webdave.de>
2024-04-04 09:35:29 +0200

    property binding

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -5,8 +5,11 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './book-card.component.html',
-  styleUrl: './book-card.component.scss'
+  styleUrl: './book-card.component.scss',
 })
 export class BookCardComponent {
-
+  myStyla = {
+    color: 'purple',
+    backgroundColor: 'orange',
+  };
 }
