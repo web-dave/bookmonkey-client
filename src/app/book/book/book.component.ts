c2320a080cfb1515977ac79954ee59c04af6148f
Dave <dave@webdave.de>
2024-04-05 11:31:54 +0200

    Inject (DI)

--- src/app/book/book/book.component.ts
+++ src/app/book/book/book.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IBook } from '../book.interface';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { NgFor } from '@angular/common';
 import { BookFilterPipe } from '../book-filter.pipe';
+import { BookApiService } from '../book-api.service';
 
 @Component({
   selector: 'app-book',
@@ -13,23 +14,12 @@ import { BookFilterPipe } from '../book-filter.pipe';
 })
 export class BookComponent {
   searchString = '';
-  books: IBook[] = [
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...',
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...',
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ...",
-    },
-  ];
+  books: IBook[] = inject(BookApiService).getAll();
+
+  // constructor(private service: BookApiService) {
+  //   this.books = service.getAll();
+  // }
+
   pong(event: IBook) {
     console.log(event);
   }
