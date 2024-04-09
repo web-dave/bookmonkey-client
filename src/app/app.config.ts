c1194ceb10a79de1c268302d8c4f9c7618b02cdf
Dave <dave@webdave.de>
2024-04-09 09:24:16 +0200

    params automatosch zuweisen

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideRouter(appRoutes)],
+  providers: [
+    provideHttpClient(),
+    provideRouter(appRoutes, withComponentInputBinding()),
+  ],
 };
--- src/app/book/book-details/book-details.component.ts
+++ src/app/book/book-details/book-details.component.ts
@@ -1,7 +1,15 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  inject,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookApiService } from '../book-api.service';
 import { AsyncPipe, JsonPipe } from '@angular/common';
+import { IBook } from '../book.interface';
+import { NEVER, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-book-details',
@@ -10,9 +18,14 @@ import { AsyncPipe, JsonPipe } from '@angular/common';
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss',
 })
-export class BookDetailsComponent {
-  snapshot = inject(ActivatedRoute).snapshot;
-  // isbn = inject(ActivatedRoute).snapshot.paramMap.get('isbn'); // Automatisch auslesen
-  isbn = inject(ActivatedRoute).snapshot.params['isbn']; // Automatisch auslesen
-  book$ = inject(BookApiService).getOne(this.isbn);
+export class BookDetailsComponent implements OnChanges {
+  @Input() isbn: string = '';
+  service = inject(BookApiService);
+  book$: Observable<IBook | undefined> = of(undefined);
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (this.isbn) {
+      this.book$ = this.service.getOne(this.isbn);
+    }
+  }
 }
