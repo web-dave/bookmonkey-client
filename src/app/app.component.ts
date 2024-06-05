1fe02939bbecbb92ff6b59560c0ed97748943e89
Dave <dave@webdave.de>
2024-06-05 13:00:18 +0200

    book-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BookCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
