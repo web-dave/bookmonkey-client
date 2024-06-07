82b0e0da52cf1113a2f5936f8c6945d4d5a8c608
Dave <dave@webdave.de>
2024-06-07 13:00:39 +0200

    httpclient

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,33 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { IBook } from './book.interface';
-import { Observable, of, from } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private books: IBook[] = [
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...',
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...',
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ...",
-    },
-  ];
-
-  constructor() {}
+  http = inject(HttpClient);
 
   getAll(): Observable<IBook[]> {
-    return of(this.books);
-    // return from(this.books);
+    return this.http.get<IBook[]>('http://localhost:4730/books');
   }
 }
