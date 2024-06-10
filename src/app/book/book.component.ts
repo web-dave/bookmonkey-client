23e70cb7d6723355f88f245535831a1a8b5e0063
Dave <dave@webdave.de>
2024-06-10 09:53:12 +0200

    Unsubscribe und OnInit

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { BookCardComponent } from './book-card/book-card.component';
 import { BookFilterPipe } from './book-filter.pipe';
 import { IBook } from './book.interface';
 import { SearchComponent } from './search/search.component';
 import { UpperCasePipe } from '@angular/common';
 import { BookService } from './book.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-book',
@@ -13,25 +14,26 @@ import { BookService } from './book.service';
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss',
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
   searchString = '';
 
-  service = inject(BookService);
+  // service = inject(BookService);
+  // dRef = inject(DestroyRef);
+
+  books$ = inject(BookService).getAll().pipe(takeUntilDestroyed());
 
   books: IBook[] = [];
 
-  constructor() {
-    this.service.getAll().subscribe({
+  ngOnInit(): void {
+    this.books$.subscribe({
       next: (data) => (this.books = data),
     });
-
-    // this.service.getAll().subscribe({
-    //   next: (data) => (this.books = data),
-    //   error: e => console.error(e)
-    // });
-    // this.service.getAll().subscribe(
-    //   (data) => (this.books = data)
-    // );
+    // this.service
+    //   .getAll()
+    //   .pipe(takeUntilDestroyed(this.dRef))
+    //   .subscribe({
+    //     next: (data) => (this.books = data),
+    //   });
   }
 
   navigate(data: IBook) {
