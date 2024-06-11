d539901f602dda2677ac9e7ed82e3b66853e88be
Dave <dave@webdave.de>
2024-06-11 11:41:11 +0200

    route order

--- src/app/book/book.routes.ts
+++ src/app/book/book.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookComponent } from './book.component';
 import { leaveGuard } from './leave.guard';
-
+// details/1234
 const bookRoutes: Routes = [
   {
     path: '',
     component: BookComponent,
     providers: [],
+  },
+  {
+    path: 'details/fav',
+    component: BookDetailsComponent,
+    canDeactivate: [leaveGuard],
   },
   {
     path: 'details/:isbn',
