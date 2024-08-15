8815b3db3733366c5666537993e370e9144759c9
Dave <dave@webdave.de>
2024-08-15 09:55:05 +0200

    Type Input

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IBook } from '../models/book.interface';
 
 @Component({
   selector: 'app-book-card',
@@ -8,8 +9,7 @@ import { Component, Input } from '@angular/core';
   styleUrl: './book-card.component.scss',
 })
 export class BookCardComponent {
-  // ToDo: get rid of any
-  @Input() content: any;
+  @Input({ required: true }) content!: IBook;
   text = 'moin';
   class = 'info';
 
