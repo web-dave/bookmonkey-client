38b41ba09f3049062643072adbf7fc350252619f
Dave <dave@webdave.de>
2024-10-29 10:31:16 +0100

    property-binding

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './book-card.component.html',
-  styleUrl: './book-card.component.scss'
+  styleUrl: './book-card.component.scss',
 })
 export class BookCardComponent {
+  customStyle = {
+    color: 'lime',
+  };
 
+  abstract = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam incidunt, optio
+  qui doloremque velit pariatu...`;
 }
