d570a39230414823c2dae4bac38f4366fb994a7e
Dave <dave@webdave.de>
2024-11-04 10:35:43 +0100

    fix track error (dublicate entries) use isbn instead

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { IBook } from './models/book.interface';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -8,23 +8,6 @@ import { HttpClient } from '@angular/common/http';
 })
 export class BookService {
   private http = inject(HttpClient);
-  private books: IBook[] = [
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...',
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...',
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ...",
-    },
-  ];
 
   getAll(): Observable<IBook[]> {
     return this.http.get<IBook[]>('https://bookmonkey-api.webdave.de/books');
--- src/app/book/models/book.interface.ts
+++ src/app/book/models/book.interface.ts
@@ -2,4 +2,5 @@ export interface IBook {
   title: string;
   author: string;
   abstract: string;
+  isbn: string;
 }
