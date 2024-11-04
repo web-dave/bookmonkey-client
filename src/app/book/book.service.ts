ba6e68791f5cbc672262c183c3f58eb050a3e7bb
Dave <dave@webdave.de>
2024-11-04 08:50:31 +0100

    create book service

---
+++ src/app/book/book.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { IBook } from './models/book.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  private books: IBook[] = [
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
+
+  getAll(): IBook[] {
+    return this.books;
+  }
+}
--- src/app/book/book/book.component.ts
+++ src/app/book/book/book.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IBook } from '../models/book.interface';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { BookFilterPipe } from '../book-filter.pipe';
 import { BookFilterComponent } from '../book-filter/book-filter.component';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book',
@@ -13,23 +14,8 @@ import { BookFilterComponent } from '../book-filter/book-filter.component';
 })
 export class BookComponent {
   searchTerm = '';
-  books: IBook[] = [
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...',
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...',
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ...",
-    },
-  ];
+  // service = inject(BookService);
+  books: IBook[] = inject(BookService).getAll();
 
   goTo(e: IBook) {
     console.table(e);
