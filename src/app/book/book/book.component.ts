f3b5535ce4aedd6f4307c2d02e072bfd8a29e6cf
Dave <dave@webdave.de>
2024-11-04 12:15:38 +0100

    Async pipe

--- src/app/book/book/book.component.ts
+++ src/app/book/book/book.component.ts
@@ -1,28 +1,28 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IBook } from '../models/book.interface';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { BookFilterPipe } from '../book-filter.pipe';
 import { BookFilterComponent } from '../book-filter/book-filter.component';
 import { BookService } from '../book.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [BookCardComponent, BookFilterPipe, BookFilterComponent],
+  imports: [BookCardComponent, BookFilterPipe, BookFilterComponent, AsyncPipe],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss',
 })
-export class BookComponent implements OnInit {
+export class BookComponent {
   searchTerm = '';
-  getBooks$ = inject(BookService).getAll().pipe(takeUntilDestroyed());
-  books: IBook[] = [];
+  books$ = inject(BookService).getAll();
+  // books: IBook[] = [];
 
-  ngOnInit(): void {
-    this.getBooks$.subscribe({
-      next: (data) => (this.books = data),
-    });
-  }
+  // ngOnInit(): void {
+  //   this.books$.subscribe({
+  //     next: (data) => (this.books = data),
+  //   });
+  // }
 
   goTo(e: IBook) {
     console.table(e);
