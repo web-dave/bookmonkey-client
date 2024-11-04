3520a7c2d9391d1223a51ace7ab9d0cf2fb07025
Dave <dave@webdave.de>
2024-11-04 13:06:40 +0100

    signal input output model

--- src/app/book/book-filter/book-filter.component.ts
+++ src/app/book/book-filter/book-filter.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  input,
+  Input,
+  model,
+  output,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-book-filter',
@@ -10,6 +18,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class BookFilterComponent {
   @Input() filter = '';
   @Output() filterChange = new EventEmitter<string>();
+
+  foo = input<string>();
+  bar = output<string>();
+
+  baz = model<string>();
 
   setFilter(filter: string) {
     this.filterChange.emit(filter);
