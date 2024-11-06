213e951a70ef3432efd8c4d72e9cb2c6e56cc01e
Dave <dave@webdave.de>
2024-11-06 09:44:01 +0100

    Guard with state (inject)

--- src/app/books/confirm-leave.guard.ts
+++ src/app/books/confirm-leave.guard.ts
@@ -1,5 +1,7 @@
 import { CanDeactivateFn } from '@angular/router';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { inject } from '@angular/core';
+import { BookService } from './book.service';
 
 export const confirmLeaveGuard: CanDeactivateFn<BookDetailsComponent> = (
   component,
@@ -8,5 +10,7 @@ export const confirmLeaveGuard: CanDeactivateFn<BookDetailsComponent> = (
   nextState,
 ) => {
   console.log(component, currentRoute, currentState, nextState);
+  const service = inject(BookService);
+  service.getOne('1').subscribe();
   return window.confirm('Sicher Diggi?');
 };
