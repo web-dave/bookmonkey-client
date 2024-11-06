82f1ac77d3cce008cc9a0e326b6f2b99e6b99e6e
Dave <dave@webdave.de>
2024-11-06 13:04:40 +0100

    Form Patch value

--- src/app/books/book-new/book-new.component.ts
+++ src/app/books/book-new/book-new.component.ts
@@ -38,6 +38,18 @@ export class BookNewComponent {
 
   reset() {
     this.newBookForm.reset();
+
+    this.newBookForm.patchValue({
+      subtitle: 'Web 2.0 Security Secrets and Solutions',
+      isbn: '9780071494618',
+      abstract:
+        'Protect your Web 2.0 architecture against the latest wave of cybercrime using expert tactics from Internet security professionals. Hacking Exposed Web 2.0 shows how hackers perform reconnaissance, choose their entry point, and attack Web 2.0 - based services, and reveals detailed countermeasures and...',
+      author: 'Rich Cannings, Himanshu Dwivedi, Zane Lackey',
+      publisher: 'McGraw-Hill',
+      price: '$12.03',
+      numPages: 258,
+      cover: 'http://localhost:4730/covers/9780071494618.png',
+    });
   }
   disable() {
     this.newBookForm.controls.title.disable();
