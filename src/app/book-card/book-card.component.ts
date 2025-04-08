033eee22387a6dfd4c923e046d9a1a5dfb34a800
Dave <dave@webdave.de>
2025-04-08 15:29:19 +0200

    property binding

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -4,8 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-book-card',
   imports: [],
   templateUrl: './book-card.component.html',
-  styleUrl: './book-card.component.scss'
+  styleUrl: './book-card.component.scss',
 })
 export class BookCardComponent {
-
+  customStyle = {
+    fontWeight: 'bold',
+  };
 }
