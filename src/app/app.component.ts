c1ab0913cdb6d860b745cb594f665310f8531f88
Dave <dave@webdave.de>
2025-04-09 11:25:13 +0200

    @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,24 @@ import { IBook } from './book.interface';
 })
 export class AppComponent {
   title = 'GFI Workshop!';
-  book: IBook = {
-    title: 'How to win friends',
-    author: 'Dale Carnegie',
-    abstract: 'In this book ...',
-  };
+
+  books: IBook[] = [
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
 
   goTo(event: IBook) {
     console.table(event);
