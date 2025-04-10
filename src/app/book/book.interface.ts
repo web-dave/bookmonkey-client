33076edd261a268fdedc54b11a065282ab1a0539
Dave <dave@webdave.de>
2025-04-10 10:41:44 +0200

    httpclient

--- src/app/book/book.interface.ts
+++ src/app/book/book.interface.ts
@@ -2,4 +2,10 @@ export interface IBook {
   title: string;
   author: string;
   abstract: string;
+  subtitle: string;
+  isbn: string;
+  publisher: string;
+  price: string;
+  numPages: number;
+  cover: string;
 }
--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,64 +1,17 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { IBook } from './book.interface';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private books: IBook[] = [
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...',
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...',
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ...",
-    },
-  ];
+  private baseUrl = 'http://localhost:4730/books';
+
+  private http = inject(HttpClient);
 
   getBooks(): Observable<IBook[]> {
-    return of(this.books);
-  }
-  myObs = obs;
-
-  constructor() {
-    obs.subscribe({
-      next: (data: any) => console.log('===>', data),
-    });
+    return this.http.get<IBook[]>(this.baseUrl);
   }
 }
-
-// Ein Observable
-const obs = {
-  subscriber: null,
-  subscribe: function (subscriber: {
-    next?: any;
-    error?: any;
-    complete?: any;
-  }) {
-    this.subscriber = subscriber as any;
-
-    // producer
-    setTimeout(() => {
-      if (!!this.subscriber) (this.subscriber as any).next('Hallo');
-    }, 2000);
-
-    // return function unsubscribe() {
-    //   this.subscriber = null;
-    // };
-  },
-  error: function () {
-    this.subscriber = null;
-  },
-  complete: function () {
-    this.subscriber = null;
-  },
-};
