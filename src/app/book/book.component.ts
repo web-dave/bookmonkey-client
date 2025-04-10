798761592d5d3c016a9943bdb4aaa13c9fd22086
Dave <dave@webdave.de>
2025-04-10 13:53:26 +0200

    signals

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { IBook } from './book.interface';
 import { BookFilterPipe } from './book-filter.pipe';
 import { BookCardComponent } from './book-card/book-card.component';
 import { BookService } from './book.service';
 import { AsyncPipe } from '@angular/common';
-import { share, shareReplay } from 'rxjs';
+import { catchError, filter, of, share, shareReplay, tap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-book',
-  imports: [BookFilterPipe, AsyncPipe],
+  imports: [BookFilterPipe, AsyncPipe, BookCardComponent],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss',
 })
@@ -17,7 +18,21 @@ export class BookComponent {
   key: 'title' | 'author' | 'abstract' = 'title';
   // books: IBook[] = [];
   service = inject(BookService); //.getBooks();
-  books$ = this.service.getBooks();
+  req$ = this.service.getBooks().pipe(share());
+  books$ = this.req$.pipe(
+    catchError((err) => of(null)),
+    filter((data) => data != null),
+  );
+
+  books = toSignal(this.books$, { initialValue: [] });
+
+  count = computed(() => this.books().length);
+
+  error$ = this.req$.pipe(
+    filter((data) => false),
+    catchError((err) => of(err.message)),
+    tap((data) => console.log(data)),
+  );
 
   goTo(event: IBook) {
     console.table(event);
