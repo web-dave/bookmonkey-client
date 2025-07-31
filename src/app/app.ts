98aeb0ddc26faebaf9c1b4fb6638b73e01a2a716
Dave <dave@webdave.de>
2025-07-31 08:54:05 +0200

    init project and check Binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'bookmonkey-client';
+  protected title = 'monkey-client V20';
 }
