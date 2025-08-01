950ce1f34cbdf49621da18c2d93b4c3638909e26
Dave <dave@webdave.de>
2025-08-01 13:08:54 +0200

    service

---
+++ src/app/book/book-api.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { IBook } from './models/book';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookApi {
+  private books: IBook[] = [
+    {
+      title: 'Moby Dick',
+      author: 'Hermans Melville',
+      abstract: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit...',
+    },
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
+
+  getAll() {
+    return this.books;
+  }
+}
--- src/app/book/book.ts
+++ src/app/book/book.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BookCard } from './book-card/book-card';
 import { BookFilter } from './book-filter-pipe';
 import { IBook } from './models/book';
+import { BookApi } from './book-api';
 
 @Component({
   selector: 'app-book',
@@ -10,28 +11,8 @@ import { IBook } from './models/book';
   styleUrl: './book.scss',
 })
 export class Book {
-  books: IBook[] = [
-    {
-      title: 'Moby Dick',
-      author: 'Hermans Melville',
-      abstract: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit...',
-    },
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...',
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...',
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ...",
-    },
-  ];
+  service = inject(BookApi);
+  books: IBook[] = this.service.getAll();
 
   searchTerm = '';
 
