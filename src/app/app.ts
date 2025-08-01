8cbf0297c5a75f5a2cb01650f7d5f8e5cdd91db0
Dave <dave@webdave.de>
2025-08-01 10:10:22 +0200

    @for

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,13 +10,33 @@ import { IBook } from './models/book';
   styleUrl: './app.scss',
 })
 export class App {
-  book: IBook = {
-    title: 'Moby Dick',
-    author: 'Hermans Melville',
-    abstract: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit...',
-  };
+  books: IBook[] = [
+    {
+      title: 'Moby Dick',
+      author: 'Hermans Melville',
+      abstract: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit...',
+    },
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...',
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...',
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ...",
+    },
+  ];
 
   navigate(book: IBook) {
     console.log(book);
   }
 }
+
+// architektur einer Angular App
+// Wie geht das (Bootstrap)
